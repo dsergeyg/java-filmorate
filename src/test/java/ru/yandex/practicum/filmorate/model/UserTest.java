c300163ef47fd5d069435989a8b892ee6c8a7d4a
Sergey Drabkin <dsergeyg85@yandex.ru>
2023-10-18 02:35:30 +0300

    Первый коммит задания 9 Filmorate ver1_6

--- src/test/java/ru/yandex/practicum/filmorate/model/UserTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/model/UserTest.java
@@ -15,20 +15,21 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UserTest {
     private static final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
     private static final Validator validator = factory.getValidator();
+
     @Test
     void validEmail() {
-        assertDoesNotThrow(() -> new User(null , "my@yandex.ru", "SomeLogin", "SomeName", LocalDate.of(2000, 12, 31)));
-        assertThrows(NullPointerException.class, () -> new User(null , null, "SomeLogin", "SomeName", LocalDate.of(2000, 12, 31)));
-        Optional<ConstraintViolation<User>> violation = validator.validate(new User(null , "myyandex.ru", "SomeLogin", "SomeName", LocalDate.of(2000, 12, 31))).stream().findFirst();
+        assertDoesNotThrow(() -> new User(null, "my@yandex.ru", "SomeLogin", "SomeName", LocalDate.of(2000, 12, 31)));
+        assertThrows(NullPointerException.class, () -> new User(null, null, "SomeLogin", "SomeName", LocalDate.of(2000, 12, 31)));
+        Optional<ConstraintViolation<User>> violation = validator.validate(new User(null, "myyandex.ru", "SomeLogin", "SomeName", LocalDate.of(2000, 12, 31))).stream().findFirst();
         assertFalse(violation.isEmpty());
         assertEquals("Email должен иметь структуру my@yandex.ru", violation.get().getMessage());
     }
 
     @Test
     void validLogin() {
-        assertDoesNotThrow(() -> new User(null , "my@yandex.ru", "SomeLogin", "SomeName", LocalDate.of(2000, 12, 31)));
-        assertThrows(NullPointerException.class, () -> new User(null , "my@yandex.ru", null, "SomeName", LocalDate.of(2000, 12, 31)));
-        Optional<ConstraintViolation<User>> violation = validator.validate(new User(null , "my@yandex.ru", "", "SomeName", LocalDate.of(2000, 12, 31))).stream().findFirst();
+        assertDoesNotThrow(() -> new User(null, "my@yandex.ru", "SomeLogin", "SomeName", LocalDate.of(2000, 12, 31)));
+        assertThrows(NullPointerException.class, () -> new User(null, "my@yandex.ru", null, "SomeName", LocalDate.of(2000, 12, 31)));
+        Optional<ConstraintViolation<User>> violation = validator.validate(new User(null, "my@yandex.ru", "", "SomeName", LocalDate.of(2000, 12, 31))).stream().findFirst();
         assertFalse(violation.isEmpty());
         assertEquals("Login должен содержать символы", violation.get().getMessage());
     }
@@ -36,23 +37,23 @@ public class UserTest {
     @Test
     void validName() {
         String name = "SomeName";
-        assertEquals(name, new User(null , "my@yandex.ru", "SomeLogin", name, LocalDate.of(2000, 12, 31)).getName());
+        assertEquals(name, new User(null, "my@yandex.ru", "SomeLogin", name, LocalDate.of(2000, 12, 31)).getName());
         String login = "SomeLogin";
-        assertEquals(login, new User(null , "my@yandex.ru", "SomeLogin", null, LocalDate.of(2000, 12, 31)).getName());
-        Optional<ConstraintViolation<User>> violation = validator.validate(new User(null , "my@yandex.ru", "SomeLogin", "", LocalDate.of(2000, 12, 31))).stream().findFirst();
+        assertEquals(login, new User(null, "my@yandex.ru", "SomeLogin", null, LocalDate.of(2000, 12, 31)).getName());
+        Optional<ConstraintViolation<User>> violation = validator.validate(new User(null, "my@yandex.ru", "SomeLogin", "", LocalDate.of(2000, 12, 31))).stream().findFirst();
         assertFalse(violation.isEmpty());
         assertEquals("Имя должно содержать символы", violation.get().getMessage());
     }
 
     @Test
     void validBirthDay() {
-        Optional<ConstraintViolation<User>> violation = validator.validate(new User(null , "my@yandex.ru", "SomeLogin", "SomeName", LocalDate.now().plusDays(1))).stream().findFirst();
+        Optional<ConstraintViolation<User>> violation = validator.validate(new User(null, "my@yandex.ru", "SomeLogin", "SomeName", LocalDate.now().plusDays(1))).stream().findFirst();
         assertFalse(violation.isEmpty());
         assertEquals("Дата рождения не может быть в будущем", violation.get().getMessage());
-        violation = validator.validate(new User(null , "my@yandex.ru", "SomeLogin", "SomeName", LocalDate.now())).stream().findFirst();
+        violation = validator.validate(new User(null, "my@yandex.ru", "SomeLogin", "SomeName", LocalDate.now())).stream().findFirst();
         assertFalse(violation.isEmpty());
         assertEquals("Дата рождения не может быть в будущем", violation.get().getMessage());
-        violation = validator.validate(new User(null , "my@yandex.ru", "SomeLogin", "SomeName", LocalDate.now().minusDays(1))).stream().findFirst();
+        violation = validator.validate(new User(null, "my@yandex.ru", "SomeLogin", "SomeName", LocalDate.now().minusDays(1))).stream().findFirst();
         assertTrue(violation.isEmpty());
     }
 
