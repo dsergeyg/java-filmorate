9fa058f6e35436c4ebb98c0aaf59307b2b2cf305
Sergey Drabkin <dsergeyg85@yandex.ru>
2023-11-08 01:34:46 +0300

    Техническое задание проекта №10 ver1_0

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -5,7 +5,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Update;
-import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.FilmService;
 import javax.validation.*;
 import java.util.*;
