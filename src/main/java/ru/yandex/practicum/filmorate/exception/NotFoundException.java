7e91bc7d5c2ecc093c407b07305cfdc1c4febca4
Sergey Drabkin <dsergeyg85@yandex.ru>
2023-11-17 01:37:29 +0300

    Техническое задание проекта №11 ver1_2

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
 
-import java.io.IOException;
-
 public class NotFoundException extends RuntimeException {
 
     public NotFoundException(String message) {
